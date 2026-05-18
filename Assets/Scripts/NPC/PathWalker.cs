c1e91608409fc35af3c83cb87c7f8f5c455c44cb
milokv <milo.kvarnberg@gmail.com>
2026-05-19 00:51:22 +0200

    adjusted pathfinding slightly

--- Assets/Scripts/NPC/PathWalker.cs
+++ Assets/Scripts/NPC/PathWalker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,8 +15,11 @@ public class PathWalker : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        proceduralGenerator.GenerateWorld();
         GameObject player1 = GameObject.FindWithTag("Player (Clone)");
-        GameObject player2 = GameObject.FindWithTag("Player (Clone)(1)"); // unused (for now) 
+        GameObject player2 = GameObject.FindWithTag("Player (Clone)(1)"); // unused (for now)
+        transform.position = new Vector3(25 * 8, transform.position.y, 25 * 8);
+        Debug.Log("moved npc to spawn");
         Vector2Int startGrid = WorldToGrid(transform.position);
         Vector2Int endGrid = WorldToGrid(player1.transform.position);
         path = PathFinder.FindShortestPath(startGrid, endGrid, proceduralGenerator.grid);
@@ -24,19 +28,26 @@ public class PathWalker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (path == null || currentPathIndex >= path.Count) return;
+        if (path == null || currentPathIndex >= path.Count)
+        {
+            Debug.Log("path == null || currentPathIndex >= path.Count");
+            return;
+        }
 
         Vector3 nextWorldPos = GridToWorld(path[currentPathIndex]);
+        Debug.Log("moving");
         transform.position = Vector3.MoveTowards(transform.position, nextWorldPos, speed * Time.deltaTime);
 
-        if (transform.position == nextWorldPos) {
+        if (Vector3.Distance(transform.position, nextWorldPos) < 0.1f)
+            {
+            Debug.Log("moved 1 step");
             currentPathIndex++;
         }
     }
 
     Vector3 GridToWorld(Vector2Int gridPos)
     {
-        return new Vector3(gridPos.x * 8, 0, gridPos.y * 8);
+        return new Vector3(gridPos.x * 8, transform.position.y, gridPos.y * 8);
     }
 
     Vector2Int WorldToGrid(Vector3 worldPos)
